e25f67194a6e70e67e4b886c20f58e634d3bc621
adarsh <adarshjayasanker5@gmail.com>
2024-06-30 07:36:54 +0530

    hbefh

--- awsS3.js
+++ awsS3.js
@@ -6,10 +6,10 @@ dotenv.config();
 
 
 const s3Client = new S3Client({
-    region: process.env.AWS_REGION,
+    region: '',
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+        accessKeyId: '',
+        secretAccessKey: ''
     }
 })
 
