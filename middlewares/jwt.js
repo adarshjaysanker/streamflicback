d547cc5d1b2441fb25efbbdc47532fcf8047db04
adarsh <adarshjayasanker5@gmail.com>
2024-07-03 09:00:34 +0530

    fve

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -1,10 +1,10 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req,res,next) => {
-    const token = req.header('Authorization').split('')[1];
+    const token = req.header('Authorization')?.split(' ')[1];
     if(!token) return res.status(401).json({error: 'Access Denied'});
     try{
-       const verified = jwt.verify(token, 'your-secret-key');
+       const verified = jwt.verify(token, 'secret');
        req.user = verified;
        next(); 
     }catch(error){
